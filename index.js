70e13c1ed33bfcf6b71209f7ecec359c8ad1f469
BonnevieAmel <amel.bonnevie@gmail.com>
2024-05-27 15:03:15 +0200

    Exercise done with bonus

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
-// First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+const { Fighter } = require("./src/Fighter");
+
+const heracles = new Fighter("🧔", "Heracles", 20, 6);
+const nemeanLion = new Fighter("🦁", "Nemean Lion", 11, 13);
+
+let round = 1;
+while (heracles.isAlive() && nemeanLion.isAlive()) {
+  console.log(`\n--- Round ${round} ---`);
+  console.log(
+    `${heracles.emoji} ${heracles.name} attacks ${nemeanLion.emoji} ${nemeanLion.name}!`
+  );
+  heracles.fight(nemeanLion);
+  console.log(
+    `${nemeanLion.emoji} ${nemeanLion.name} has ${nemeanLion.life} life points left.`
+  );
+
+  if (!nemeanLion.isAlive()) {
+    console.log(
+      `${heracles.emoji} ${heracles.name} has defeated ${nemeanLion.emoji} ${nemeanLion.name}!`
+    );
+    break;
+  }
+
+  console.log(
+    `${nemeanLion.emoji} ${nemeanLion.name} attacks ${heracles.emoji} ${heracles.name}!`
+  );
+  nemeanLion.fight(heracles);
+  console.log(
+    `${heracles.emoji} ${heracles.name} has ${heracles.life} life points left.`
+  );
+
+  if (!heracles.isAlive()) {
+    console.log(
+      `${nemeanLion.emoji} ${nemeanLion.name} has defeated ${heracles.emoji} ${heracles.name}!`
+    );
+  }
+
+  round++;
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,26 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100;
+
+class Fighter {
+  constructor(emoji, name, strength, dexterity) {
+    this.emoji = emoji;
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+
+  isAlive() {
+    return this.life > 0;
+  }
+
+  fight(enemy) {
+    const damage = Math.floor(Math.random() * this.strength) + 1;
+    const actualDamage = Math.max(damage - enemy.dexterity, 0);
+    enemy.life = Math.max(enemy.life - actualDamage, 0);
+    console.log(`${enemy.emoji} ${enemy.name} (${enemy.life} life points)`);
+  }
+}
+
+module.exports = { Fighter };
